3bda6733cd66856873be1832e3e3fc4938501c26
Zakir123164 <jkrhossain8@gamil.com>
2025-12-15 08:53:10 +0600

    update

--- app/component/Banner.jsx
+++ app/component/Banner.jsx
@@ -1,31 +1,21 @@
 "use client";
-import Image from "next/image";
 
+import bannerBg from '../../public/bannerlambo1.png'
 
 
 
 
 const HeroSection = () => {
   return (
-    <section className="w-full bg-[#f5f5f5] pb-20">
+    <section  className="w-full banner  pb-20">
       {/* HERO TOP PART */}
       <div>
       <div className="w-full max-w-7xl mx-auto px-5 lg:px-0 flex flex-col-reverse lg:flex-row items-center gap-10 py-10">
 
+  
       
-        {/* LEFT TEXT */}
-        {/* <div className="flex-1"> */}
-      
-                 {/* RIGHT IMAGE */}
-        <div className="relative">
-          <Image
-            src="/banner lambo 1.png"
-            width={1980}
-            height={450}
-            alt="Lamborghini"            
-            className="drop-shadow-xl object-contain"
-          />
-        </div>
+                 {/* banner IMAGE */}
+        
 
           <p className="mt-5 text-xl font-semibold text-black">
             Safer, Faster And Comfortable
